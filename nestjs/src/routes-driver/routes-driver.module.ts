980d8af0e362e7837a671afb7a30fc6bad223537
Jonathan Vinicius <jonathanvinicius211@gmail.com>
2024-02-10 15:29:02 -0300

    refactor: adding new dependencies in routes driver module

--- nestjs/src/routes-driver/routes-driver.module.ts
+++ nestjs/src/routes-driver/routes-driver.module.ts
@@ -3,10 +3,13 @@ import { RouteDriver } from './models/routes-driver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoutesDriverController } from './routes-driver.controller';
 import { RoutesDriverService } from './routes-driver.service';
+import { RoutesModule } from 'src/routes/routes.module';
+import { Route } from 'src/routes/models/routes';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RouteDriver])],
+  imports: [TypeOrmModule.forFeature([RouteDriver, Route]), RoutesModule],
   controllers: [RoutesDriverController],
   providers: [RoutesDriverService],
+  exports: [RoutesDriverService],
 })
 export class RoutesDriverModule {}
