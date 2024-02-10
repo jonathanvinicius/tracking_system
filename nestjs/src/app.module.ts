8a8e8396f02998e72840bf69f3a31f072a28b38d
Jonathan Vinicius <jonathanvinicius211@gmail.com>
2024-02-10 15:37:25 -0300

    refactor: injecting bull and redis dependencies

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -4,6 +4,10 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { MapsModule } from './maps/maps.module';
 import { RoutesDriverModule } from './routes-driver/routes-driver.module';
+import { BullModule } from '@nestjs/bull';
+import { BullBoardModule } from '@bull-board/nestjs';
+import { ExpressAdapter } from '@bull-board/express';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
   imports: [
@@ -12,8 +16,18 @@ import { RoutesDriverModule } from './routes-driver/routes-driver.module';
     DatabaseModule,
     MapsModule,
     RoutesDriverModule,
+    PrometheusModule.register(),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullBoardModule.forRoot({
+      route: '/queues',
+      adapter: ExpressAdapter,
+    }),
   ],
   controllers: [],
-  providers: [],
 })
 export class AppModule {}
