80d3a40c63d616da090f3da0f09bbb1390196dd6
Jonathan Vinicius <jonathanvinicius211@gmail.com>
2024-02-12 14:16:49 -0300

    refactor: changing variable for db

--- nestjs/typeorm.config.ts
+++ nestjs/typeorm.config.ts
@@ -1,18 +1,18 @@
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
 
 config();
 
-const configService = new ConfigService();
+// const configService = new ConfigService();
 
 export default new DataSource({
   type: 'mysql',
-  host: configService.getOrThrow('34.95.150.244'),
-  port: configService.getOrThrow('3306'),
-  database: configService.getOrThrow('tracking_system'),
-  username: configService.getOrThrow('jonathan'),
-  password: configService.getOrThrow('78951Root.'),
+  host: '34.95.150.244',
+  port: 3306,
+  database: 'tracking_system',
+  username: 'jonathan',
+  password: '78951Root.',
   migrations: ['src/migrations/**/*.ts'],
   entities: [],
 });
